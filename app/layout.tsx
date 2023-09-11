7480907cbcf72991239c69d8e792e281521daa5c
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2023-09-11 12:49:02 -0300

    added new maskable icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,37 +2,48 @@ import './globals.css'
 import { Jost } from 'next/font/google'
 import NavBar from '@/components/common/NavBar'
 import { Analytics } from '@vercel/analytics/react';
-import { get } from '@vercel/edge-config';
-import { onDevEnv } from '@/lib/utils';
 
 const jost = Jost({ subsets: ['latin'] })
 
 export const metadata = {
   manifest: "/manifest.json",
   themeColor: "#ffffff",
+  favicon: "/favicon.ico",
+  title: "Aliento para el viaje",
+  description: "Aliento para el viaje es un podcast que busca inspirar a los oyentes a través de la Palabra de Dios.",
+  keywords: "podcast, cristiano, devocional, inspiración, aliento, viaje, diario, biblia, palabra, dios, jesús, espíritu, santo, evangelio, iglesia, cristianismo, religión, oración, fe, esperanza, amor, paz, gozo, salvación, gracia, misericordia, perdón, arrepentimiento, reconciliación, justicia, verdad, vida, eterna, cielo, infierno, muerte, cruz, sangre, cristo, reino, cielo, infierno, diablo, demonio, ángel, ángeles, creación, pecado, hombre, mujer, matrimonio, familia, hijos, padres, abuelos, nietos, hermanos, hermanas, amigos, amigas, enemigos, enemigas, mundo, universo, tiempo, espacio, eternidad, pasado, presente, futuro, ayer, hoy, mañana, hoy, ahora, siempre",
+
+  // Open Graph
+  og: {
+    type: "website",
+    locale: "es_ES",
+    url: "https://alientoparaelviaje.com",
+    title: "Aliento para el viaje",
+    description: "Aliento para el viaje es un podcast que busca inspirar a los oyentes a través de la Palabra de Dios.",
+    image: "/public/maskable_icon.png",
+    site_name: "Aliento para el viaje",
+    imageWidth: 512,
+    imageHeight: 512,
+  },
+
+  // Twitter
+  twitter: {
+    handle: "@apv_podcast",
+    site: "@apv_podcast",
+    cardType: "summary_large_image",
+  },
 };
-
-
-async function shouldShowNotificationButton(): Promise<boolean> {
-  if (onDevEnv()) {
-    return true;
-  } else {
-    const notificationButtonFlag = await get('notificationButton');
-    return notificationButtonFlag === 'true'
-  }
-}
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const notificationButton = await shouldShowNotificationButton()
   return (
     <html lang="es" >
       <body className={jost.className}>
         <NavBar
-          notificationButton={notificationButton}
+          notificationButton={true}
           dark={false}
         />
         {children}
